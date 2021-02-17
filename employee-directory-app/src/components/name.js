345d4a5ae78b072cb04d0fb92f62eed5b8f28db8
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-17 01:25:14 -0800

    Added the react and export with function.

--- employee-directory-app/src/components/name.js
+++ employee-directory-app/src/components/name.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function Name(props) {
+    return (
+        <div className="text-center">
+            {props.first}  {props.last}
+        </div>
+    )
+}
+
+export default Name;
--- employee-directory-app/src/components/phone.js
+++ employee-directory-app/src/components/phone.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+function Phone(props) {
+    return (
+        <div 
+        className="text-center">
+            {props.number}
+        </div>
+
+    )
+}
+
+export default Phone;
