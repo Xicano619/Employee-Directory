b45ce73710f97e007a051e705bd3332726e40172
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-21 20:50:08 -0800

    Added the searchbox with a searchbox css.

---
+++ employee-directory-app/src/components/SearchBox/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SearchBox.css";
+
+function SearchBox({ handleSearchChange }) {
+  return (
+    <div className="searchbox">
+      <form className="form-inline">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={e => handleSearchChange(e)}
+        />
+      </form>
+    </div>
+  );
+}
+export default SearchBox;
