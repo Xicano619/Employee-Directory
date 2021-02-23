54966b37484c7cef2fce1b0a27381cfdf8b22c2b
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-22 20:39:40 -0800

    Adding functions... only Header is showing.

--- employee-directory-app/src/App.js
+++ employee-directory-app/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from './components/Container/index';
+// import Main from "./components/Main";
+// import Wrapper from "./components/Wrapper";
+import Header from "./components/Header/index";
+
+// import "./App.css";
 
 function App() {
-  return < />; 
-  
+  return (
+    <div className="App">
+      <Container>
+        <Header />
+        {/* <Main /> */}
+      </Container>
+    </div>
+  );
 }
 
 export default App;
