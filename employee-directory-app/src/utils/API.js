13ec83fe8468862c21dbe89afe4359476a583113
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-24 00:22:31 -0800

    Changed search () to getUsers ().

--- employee-directory-app/src/utils/API.js
+++ employee-directory-app/src/utils/API.js
@@ -6,7 +6,7 @@ const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-    search: function() {
+    getUsers: function() {
         return axios.get(BASEURL);
     }
 }
